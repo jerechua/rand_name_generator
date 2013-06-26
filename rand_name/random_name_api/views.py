bc2cebbae218c244e288066521f2d81248c932ed
Jeremy <jere.chua@gmail.com>
2013-06-25 21:58:19 -0400

    changed randomizer

--- rand_name/random_name_api/views.py
+++ rand_name/random_name_api/views.py
@@ -3,52 +3,34 @@ from random import random, randint
 from rest_framework import views, serializers
 from rest_framework.response import Response
 
-
-letter_histogram = (('a', 8.167),
-                   ('b', 1.492),
-                   ('c', 2.782),
-                   ('d', 4.253),
-                   ('e', 12.70),
-                   ('f', 2.228),
-                   ('g', 2.015),
-                   ('h', 6.094),
-                   ('i', 6.966),
-                   ('j', 0.153),
-                   ('k', 0.772),
-                   ('l', 4.025),
-                   ('m', 2.406),
-                   ('n', 6.749),
-                   ('o', 7.507),
-                   ('p', 1.929),
-                   ('q', 0.095),
-                   ('r', 5.987),
-                   ('s', 6.327),
-                   ('t', 9.056),
-                   ('u', 2.758),
-                   ('v', 0.978),
-                   ('w', 2.360),
-                   ('x', 0.150),
-                   ('y', 1.974),
-                   ('z', 0.074))
+from random_name_generator.views import letter_histogram, conditional_histogram
 
 
 class RandNameGeneratorView(views.APIView):
 
+    ascii_a = 97
 
-    def generate_letter(self):
-        dist_sum = random() * 100
+    def generate_letter(self, last_letter):
+        dist_sum = random()
 
-        for letter_stat in letter_histogram:
-            dist_sum -= letter_stat[1]
+        letter_index = ord(last_letter) - self.ascii_a
+
+        index = 0
+        for letter_stat in conditional_histogram[letter_index]:
+            dist_sum -= letter_stat
             if dist_sum < 0:
-                return letter_stat[0]
+                return chr(index + self.ascii_a)
+            index += 1
 
 
     def generate_name(self, name_length):
 
         name = ""
+        letter = chr(randint(0, 26) + self.ascii_a)
+
         for i in range(name_length):
-            name += self.generate_letter()
+            name += letter
+            letter = self.generate_letter(letter)
 
         return name
 
